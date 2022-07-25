27052df641faca43b1c4a724b91cf292367f006e
tony <tonychi123@gmail.com>
2022-07-25 15:43:11 +0800

    fix: catagory 3 name lossed

--- analyse.py
+++ analyse.py
@@ -13,19 +13,20 @@ def cut(d):
     assert any([re.search(ed,d) for ed in end]),d+'=> Input not end with \''+'\' or \''.join(end)+'\''
 
     cutIdx=d.find('第')
-    cata=d[:cutIdx]
+    name=d[:cutIdx]
+    cata=name
     if cata!=catagory[0] and cata!=catagory[1]:
         cata=catagory[2]
-    return cata,str2list(d[cutIdx:])
+    return cata,name,str2list(d[cutIdx:])
      
 
 def analyse(zp):
     dic={catagory[0]:{},catagory[1]:{},catagory[2]:{}}
     for c,rs in zp:
         for r in rs:
-            main,lst=cut(r) 
+            main,name,lst=cut(r) 
             for lt in lst:
-                lt=main+lt
+                lt=name+lt
                 if lt not in dic[main]:
                     dic[main][lt]=[]
                 dic[main][lt].append(c)
