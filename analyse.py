69d81290c9140fea7b150caf89b3563948e330d7
tony <tonychi123@gmail.com>
2022-07-25 15:40:38 +0800

    feat: change analyse input to 2d array

--- analyse.py
+++ analyse.py
@@ -1,30 +1,34 @@
 import re
 
+catagory=['刑法','刑事訴訟法','其他']
+
 def str2list(ditiao):
 #    assert re.search('^第.*條。$',ditiao),'str2list not expected format'
     return ditiao.split('、')
 
-def analyse(data):
+def cut(d):
     end=['法','條例','公約']
-    catagory=['刑法','刑事訴訟法','其他']
-    dic={catagory[0]:{},catagory[1]:{},catagory[2]:{}}
-    for d in data:
+    d=d.replace(' ','')
+#    assert d.endswith('。'),'string does not end with\'。\''
+    assert any([re.search(ed,d) for ed in end]),d+'=> Input not end with \''+'\' or \''.join(end)+'\''
 
-        d=d.replace(' ','')
-#        assert d.endswith('。'),'string does not end with\' 。\''
-        assert any([re.search(ed,d) for ed in end]),d+'=> Input not end with \''+'\' or \''.join(end)+'\''
-
-        cutIdx=d.find('第')
-        main=d[:cutIdx]
-        if main!=catagory[0] and main!=catagory[1]:
-            main=catagory[2]
-        
-        lst=str2list(d[cutIdx:])
-        for lt in lst:
-            lt=d[:cutIdx]+lt
-            dic[main][lt]=dic[main].get(lt,0)+1
-    
+    cutIdx=d.find('第')
+    cata=d[:cutIdx]
+    if cata!=catagory[0] and cata!=catagory[1]:
+        cata=catagory[2]
+    return cata,str2list(d[cutIdx:])
      
+
+def analyse(zp):
+    dic={catagory[0]:{},catagory[1]:{},catagory[2]:{}}
+    for c,rs in zp:
+        for r in rs:
+            main,lst=cut(r) 
+            for lt in lst:
+                lt=main+lt
+                if lt not in dic[main]:
+                    dic[main][lt]=[]
+                dic[main][lt].append(c)
     return dic
 
     
