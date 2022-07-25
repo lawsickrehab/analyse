d7790dd324e23a8a2fdd7fccd5618cb6173a6630
tony <tonychi123@gmail.com>
2022-07-25 16:07:01 +0800

    feat: dicsum function

---
+++ utils.py
@@ -0,0 +1,13 @@
+from constants import *
+
+
+def dicsum(dics):
+    ret={catagories[0]:{},catagories[1]:{},catagories[2]:{}}
+    for dic in dics:
+        for cat in catagories:
+            for ref,lst in dic[cat].items():
+                if ref not in ret[cat]:
+                    ret[cat][ref]=[]
+                ret[cat][ref].extend(lst)
+    return ret
+            
