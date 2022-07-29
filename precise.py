f1b46210d4011e3c496dc4b0405359df7454d29e
tony <tonychi123@gmail.com>
2022-07-29 14:26:08 +0800

    refactor: read contents from txt

--- precise.py
+++ precise.py
@@ -14,32 +14,21 @@ from trans import pdf2txt,dic2csv
 from analyse import analyse
 from filters import find_by_regex,contents
 
-dataPath='./data/'
+dataPath='../logic-graph/txt/'
 ls=os.listdir(dataPath)
 logfile=open("log",'w')
 
 # 
-broken=["105年度刑事具有參考價值之裁判要旨暨裁判全文.pdf","最高法院刑事具有參考價值之裁判要旨暨裁判全文（109年度11月）.pdf"]
-
-# content count failed
-strange=["109年度（6月）刑事具有參考價值之裁判要旨暨裁判全文.pdf","109年度（7月）刑事具有參考價值之裁判要旨暨裁判全文.pdf"]
+broken=["最高法院刑事具有參考價值之裁判要旨暨裁判全文（109年度11月）.txt"]
 
 st=set()
 st={"109,台上,1478","109,台上,1479","109,台上,2076","109,台上,2077"}
 
 for fname in tqdm(ls):
-    if fname in broken or fname in strange:
-        print(Fore.RED+   "File not expected "+Fore.LIGHTBLACK_EX+"|"+Fore.RESET,fname,file=logfile)
+    if fname in broken or 0:
+        print(Fore.RED+   "File not expected "+Fore.LIGHTBLACK_EX+"|"+Fore.RESET,fname)
         continue
-    try:
-        text=pdf2txt(dataPath+fname) 
-        print(Fore.GREEN+ "Read successful   "+Fore.LIGHTBLACK_EX+"|"+Fore.RESET,fname,file=logfile)
-    except KeyboardInterrupt:
-        print("KeyboardInterrupted")
-        break
-    except:
-        print(Fore.YELLOW+"Read failed       "+Fore.LIGHTBLACK_EX+"|"+Fore.RESET,fname,file=logfile)
-        continue
+    text=open(dataPath+fname,'r').read()
     
     cnts=contents(text)
     cts=[filters.prec(c) for c in cnts if c!="NIL"]
@@ -48,7 +37,9 @@ for fname in tqdm(ls):
 
 judgePath='../judgements/最高法院_刑事/'
 jls=os.listdir(judgePath)
-outfile=open("precisetest.txt",'w')
+outfile=open("precise.txt",'w')
+
+print(len(st),"found in st")
 
 for jname in tqdm(jls):
     with open(judgePath+jname,'r') as jfile:
@@ -58,5 +49,5 @@ for jname in tqdm(jls):
             st.remove(cur["no"])
             print(jname,file=outfile)
 
-print("st reamins",len(st))
+print("st still reamins",len(st))
 print(st)
